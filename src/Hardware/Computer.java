3955f14a0a0d53fa796ebf0c1253726a96487689
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-04-21 08:43:16 +0200

    write computerclass

--- src/Hardware/Computer.java
+++ src/Hardware/Computer.java
@@ -1,4 +1,23 @@
 package Hardware;
 
 public class Computer {
+	private Memory memory;
+	private Program program;
+	private ProgramCounter pc;
+	
+	public Computer(Memory memory){
+		this.memory = memory;
+		pc = new ProgramCounter();
+	}
+	
+	public void load(Program program){
+		this.program = program;
+		program.setContext(memory);
+	}
+	
+	public void run(){
+		while(ProgramCounter.getPC() >= 0){
+			program.executeLine(pc);
+		}
+	}
 }

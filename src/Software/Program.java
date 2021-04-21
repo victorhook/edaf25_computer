c9d7aab0965036860efb8000f47051a8dae53c5e
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-04-21 09:36:24 +0200

    Update Program.java

--- src/Software/Program.java
+++ src/Software/Program.java
@@ -1,4 +1,18 @@
 package Software;
 
 public class Program {
+	import Software.Instruction;
+	import java.util.ArrayList;
+
+	public class Program extends ArrayList<Instruction> {
+	    ProgramCounter pc= new ProgramCounter();
+
+	    public void execute(Memory memory) {
+	        
+	        while(pc.getPc()!=-1) {
+	        get(pc.getPc()).execute(memory);
+	    }
+
+	    }
+	}
 }

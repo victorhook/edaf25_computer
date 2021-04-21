93f3770ebd8cd5e0ff551df565b7896cf7cd64a4
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-04-21 09:33:06 +0200

    update memory & operand

--- src/Hardware/Memory.java
+++ src/Hardware/Memory.java
@@ -15,6 +15,13 @@ public abstract class Memory {
     public void write(int addr, Word value) {
         mem[addr] = getWord(value);
     }
+    
+    public Word get(int index){
+		if(index > memory.size || index < 0){
+			return null;
+		}else{
+			return memory[index];
+		}
 
     protected abstract Word getWord(Word word);
 
--- src/Hardware/Operand.java
+++ src/Hardware/Operand.java
@@ -3,6 +3,6 @@ package Hardware;
 public interface Operand {
 
 	public Integer Value();
-	
+	public Word getWord(Memory context);
 	public String print();
 }

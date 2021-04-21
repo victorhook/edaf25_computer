6fd794258a23d5510d21f076509b72c664aa9d64
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-04-21 08:59:49 +0200

    write pc and adress

--- src/Hardware/Address.java
+++ src/Hardware/Address.java
@@ -1,4 +1,13 @@
 package Hardware;
 
 public class Address {
+private int addr;
+	
+	public Address(int addr){
+		this.addr = addr;
+	}
+	public String print() {
+		return "[" + addr + "]";
+	}
 }
+
--- src/Hardware/ProgramCounter.java
+++ src/Hardware/ProgramCounter.java
@@ -1,4 +1,17 @@
 package Hardware;
 
 public class ProgramCounter {
+	 private Integer pc;
+
+	    public ProgramCounter() {
+	        pc++;
+
+	    }
+	    public Integer getPc() {
+	        return pc;
+
+	    }
+	    public Integer setPc(int pc) {
+	        return  this.pc=pc;
+	    }
 }

e793584e639cb3fb2fefc3f7f3965f08baf93a07
abdtaha <78154799+abdtaha@users.noreply.github.com>
2021-04-21 09:12:51 +0200

    Update Address.java

--- src/Hardware/Address.java
+++ src/Hardware/Address.java
@@ -5,7 +5,14 @@ private int addr;
 	
 	public Address(int addr){
 		this.addr = addr;
+		
 	}
+
+	@Override
+	public Integer value() {
+		return
+	}
+	@Override
 	public String print() {
 		return "[" + addr + "]";
 	}
